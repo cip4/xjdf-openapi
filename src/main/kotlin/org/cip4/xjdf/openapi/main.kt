b7bf57f1216d797768bf5fda3d0f9ee1b6bee9d0
Michel Hartmann <michel.hartmann@flyeralarm.com>
2021-04-26 21:10:03 +0200

    Don't print generated spec to STDOUT

--- src/main/kotlin/org/cip4/xjdf/openapi/main.kt
+++ src/main/kotlin/org/cip4/xjdf/openapi/main.kt
@@ -4,6 +4,5 @@ import java.io.FileOutputStream
 
 fun main() {
     val converter = OpenApiConverter(OpenApiConverter::class.java.getResourceAsStream("/xjdf.xsd"))
-    val tee = TeeOutputStream(System.out, FileOutputStream("build/resources/main/xjdf.yml"))
-    converter.convert(tee, true)
+    converter.convert(FileOutputStream("build/resources/main/xjdf.yml"), true)
 }
