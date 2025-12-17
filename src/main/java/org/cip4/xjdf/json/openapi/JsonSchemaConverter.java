7c41b5e9f94db87a0f86d1856ef86e38b2c86e99
Rainer Prosi <rainer.prosi@heidelberg.com>
2025-12-17 14:45:09 +0100

    added support for pruned xml schema

--- src/main/java/org/cip4/xjdf/json/openapi/JsonSchemaConverter.java
+++ src/main/java/org/cip4/xjdf/json/openapi/JsonSchemaConverter.java
@@ -1,18 +1,5 @@
 package org.cip4.xjdf.json.openapi;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import org.cip4.xjdf.json.openapi.model.Model;
-import org.cip4.xjdf.json.openapi.model.Schema;
-import org.cip4.xjdf.json.openapi.model.Schemas;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -20,194 +7,243 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
-public class JsonSchemaConverter {
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 
-    public static final String PREFIX = "#/$defs/";
-    private final Document doc;
-    private final XPath xPath;
+import org.cip4.xjdf.json.openapi.model.Model;
+import org.cip4.xjdf.json.openapi.model.Schema;
+import org.cip4.xjdf.json.openapi.model.Schemas;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-    public static final String XJDF_SCHEMA = "https://schema.cip4.org/jdfschema_2_2/xjdf.json";
-    public static final String XJMF_SCHEMA = "https://schema.cip4.org/jdfschema_2_2/xjmf.json";
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
-    public JsonSchemaConverter(InputStream sourceXsd) {
-        SchemaReader reader = new SchemaReader();
-        this.doc = reader.readXml(sourceXsd);
-        this.xPath = reader.getXPath();
-    }
+public class JsonSchemaConverter
+{
 
-    public Schemas convert(OutputStream xjdfOutputStream, OutputStream xjmfOutputStream) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
+	public static final String PREFIX = "#/$defs/";
+	private final Document doc;
+	private final XPath xPath;
 
-        ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+	public static final String XJDF_SCHEMA = "https://schema.cip4.org/jdfschema_2_2/xjdf.json";
+	public static final String XJMF_SCHEMA = "https://schema.cip4.org/jdfschema_2_2/xjmf.json";
 
-        Schema xjdfSchema = convertModel("XJDF");
-        xjdfSchema.id("https://schema.cip4.org/jdfschema_2_2/xjdf.json");
+	public JsonSchemaConverter(InputStream sourceXsd)
+	{
+		final SchemaReader reader = new SchemaReader();
+		this.doc = reader.readXml(sourceXsd);
+		this.xPath = reader.getXPath();
+	}
 
-        try (OutputStreamWriter writer = new OutputStreamWriter(xjdfOutputStream, StandardCharsets.UTF_8)) {
-            writer.write(objectWriter.writeValueAsString(xjdfSchema));
-        } catch (Exception e) {
-            throw new RuntimeException("Error writing XJDF schema", e);
-        }
+	public Schemas convert(OutputStream xjdfOutputStream, OutputStream xjmfOutputStream)
+	{
+		final ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
 
-        Map<String, String> xjdfTypes = new HashMap<>();
-        xjdfSchema.defs().forEach((key, value) -> xjdfTypes.put(key, xjdfSchema.id() + PREFIX + key));
+		final ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+		final Schemas schemaMap = new Schemas();
+		final Map<String, String> xjdfTypes = new HashMap<>();
 
-        Schema xjmfSchema = convertModel("XJMF", xjdfTypes);
-        xjmfSchema.id("https://schema.cip4.org/jdfschema_2_2/xjmf.json");
+		if (xjdfOutputStream != null)
+		{
+			final Schema xjdfSchema = convertModel("XJDF");
+			xjdfSchema.id("https://schema.cip4.org/jdfschema_2_2/xjdf.json");
 
-        try (OutputStreamWriter writer = new OutputStreamWriter(xjmfOutputStream, StandardCharsets.UTF_8)) {
-            writer.write(objectWriter.writeValueAsString(xjmfSchema));
-        } catch (Exception e) {
-            throw new RuntimeException("Error writing XJMF schema", e);
-        }
+			try (OutputStreamWriter writer = new OutputStreamWriter(xjdfOutputStream, StandardCharsets.UTF_8))
+			{
+				writer.write(objectWriter.writeValueAsString(xjdfSchema));
+			}
+			catch (final Exception e)
+			{
+				throw new RuntimeException("Error writing XJDF schema", e);
+			}
+			schemaMap.put("XJDF", xjdfSchema);
+			xjdfSchema.defs().forEach((key, value) -> xjdfTypes.put(key, xjdfSchema.id() + PREFIX + key));
+		}
 
-        Schemas schemaMap = new Schemas();
-        schemaMap.put("XJDF", xjdfSchema);
-        schemaMap.put("XJMF", xjmfSchema);
+		if (xjmfOutputStream != null)
+		{
+			final Schema xjmfSchema = convertModel("XJMF", xjdfTypes);
+			xjmfSchema.id("https://schema.cip4.org/jdfschema_2_2/xjmf.json");
 
-        return schemaMap;
-    }
+			try (OutputStreamWriter writer = new OutputStreamWriter(xjmfOutputStream, StandardCharsets.UTF_8))
+			{
+				writer.write(objectWriter.writeValueAsString(xjmfSchema));
+			}
+			catch (final Exception e)
+			{
+				throw new RuntimeException("Error writing XJMF schema", e);
+			}
 
-    public Schema convertModel(String root) {
-        return convertModel(root, new HashMap<>());
-    }
+			schemaMap.put("XJMF", xjmfSchema);
+		}
+		return schemaMap;
+	}
 
-    public Schema convertModel(String root, Map<String, String> referencedTypes) {
-        TypeTranslator nameTranslator = new TypeTranslator(PREFIX, referencedTypes);
-        Schemas defs = new Schemas();
-        convertNamedElement(root, nameTranslator, defs);
-        Schema xjdfSchema = defs.remove(root);
-        xjdfSchema.schema("https://json-schema.org/draft/2020-12/schema");
-        xjdfSchema.defs(defs);
-        return xjdfSchema;
-    }
+	public Schema convertModel(String root)
+	{
+		return convertModel(root, new HashMap<>());
+	}
 
-    private void convertNamedType(String typeName, TypeTranslator nameTranslator, Schemas schemas) {
-        if (schemas.containsKey(typeName) || nameTranslator.isExternalType(typeName)) {
-            return;
-        }
+	public Schema convertModel(String root, Map<String, String> referencedTypes)
+	{
+		final TypeTranslator nameTranslator = new TypeTranslator(PREFIX, referencedTypes);
+		final Schemas defs = new Schemas();
+		convertNamedElement(root, nameTranslator, defs);
+		final Schema xjdfSchema = defs.remove(root);
+		xjdfSchema.schema("https://json-schema.org/draft/2020-12/schema");
+		xjdfSchema.defs(defs);
+		return xjdfSchema;
+	}
 
-        try {
-            Node typeNode = (Node) xPath.evaluate(
-                "/xs:schema/xs:simpleType[@name='" + typeName + "'] | /xs:schema/xs:complexType[@name='" + typeName + "']",
-                doc,
-                XPathConstants.NODE
-            );
+	private void convertNamedType(String typeName, TypeTranslator nameTranslator, Schemas schemas)
+	{
+		if (schemas.containsKey(typeName) || nameTranslator.isExternalType(typeName))
+		{
+			return;
+		}
 
-            if (typeNode.getLocalName().equals("simpleType")) {
-                convertSimpleType(typeNode, nameTranslator, schemas);
-            } else {
-                convertComplexType(typeNode, nameTranslator, schemas);
-            }
-        } catch (XPathExpressionException e) {
-            throw new RuntimeException("Error evaluating XPath for named type: " + typeName, e);
-        }
-    }
+		try
+		{
+			final Node typeNode = (Node) xPath.evaluate(
+					"/xs:schema/xs:simpleType[@name='" + typeName + "'] | /xs:schema/xs:complexType[@name='" + typeName + "']", doc, XPathConstants.NODE);
 
-    private void convertSimpleType(Node typeNode, TypeTranslator nameTranslator, Schemas schemas) {
-        Model schema = SimpleType.create(
-            typeNode,
-            new Context(xPath, nameTranslator, typeNode)
-        ).getModel();
+			if ("simpleType".equals(typeNode.getLocalName()))
+			{
+				convertSimpleType(typeNode, nameTranslator, schemas);
+			}
+			else
+			{
+				convertComplexType(typeNode, nameTranslator, schemas);
+			}
+		}
+		catch (final XPathExpressionException e)
+		{
+			throw new RuntimeException("Error evaluating XPath for named type: " + typeName, e);
+		}
+	}
 
-        schemas.put(schema.getName(), schema.getSchema());
-        convertExtendedTypes(typeNode, nameTranslator, schemas);
-    }
+	private void convertSimpleType(Node typeNode, TypeTranslator nameTranslator, Schemas schemas)
+	{
+		final Model schema = SimpleType.create(typeNode, new Context(xPath, nameTranslator, typeNode)).getModel();
 
-    private void convertComplexType(Node typeNode, TypeTranslator nameTranslator, Schemas schemas) {
-        Model schema = new ComplexType(new Context(xPath, nameTranslator, typeNode)).getModel();
-        schemas.put(schema.getName(), schema.getSchema());
+		schemas.put(schema.getName(), schema.getSchema());
+		convertExtendedTypes(typeNode, nameTranslator, schemas);
+	}
 
-        convertReferencedElements(typeNode, nameTranslator, schemas);
-        convertReferencedTypes(typeNode, nameTranslator, schemas);
-        convertExtendedTypes(typeNode, nameTranslator, schemas);
-    }
+	private void convertComplexType(Node typeNode, TypeTranslator nameTranslator, Schemas schemas)
+	{
+		final Model schema = new ComplexType(new Context(xPath, nameTranslator, typeNode)).getModel();
+		schemas.put(schema.getName(), schema.getSchema());
 
-    private void convertNamedElement(String elementName, TypeTranslator nameTranslator, Schemas schemas) {
-        try {
-            Node element = (Node) xPath.evaluate(
-                "/xs:schema/xs:element[@name='" + elementName + "']",
-                doc,
-                XPathConstants.NODE
-            );
+		convertReferencedElements(typeNode, nameTranslator, schemas);
+		convertReferencedTypes(typeNode, nameTranslator, schemas);
+		convertExtendedTypes(typeNode, nameTranslator, schemas);
+	}
 
-            convertReferencedTypes(element, nameTranslator, schemas);
-            convertSubstituteElements(elementName, nameTranslator, schemas);
+	private void convertNamedElement(String elementName, TypeTranslator nameTranslator, Schemas schemas)
+	{
+		try
+		{
+			final Node element = (Node) xPath.evaluate("/xs:schema/xs:element[@name='" + elementName + "']", doc, XPathConstants.NODE);
 
-            if ((
-                element.getAttributes().getNamedItem("abstract") != null
-                    && element.getAttributes().getNamedItem("abstract").getNodeValue().equals("true")
-            )
-                || element.getAttributes().getNamedItem("substitutionGroup") != null
-            ) {
-                return;
-            }
+			convertReferencedTypes(element, nameTranslator, schemas);
+			convertSubstituteElements(elementName, nameTranslator, schemas);
 
-            Model schema = new NamedElement(new Context(xPath, nameTranslator, element)).getModel();
-            schemas.put(schema.getName(), schema.getSchema());
+			if ((element.getAttributes().getNamedItem("abstract") != null && "true".equals(element.getAttributes().getNamedItem("abstract").getNodeValue()))
+					|| element.getAttributes().getNamedItem("substitutionGroup") != null)
+			{
+				return;
+			}
 
-            convertReferencedElements(element, nameTranslator, schemas);
-            convertExtendedTypes(element, nameTranslator, schemas);
-        } catch (XPathExpressionException e) {
-            throw new RuntimeException("Error evaluating XPath for named element: " + elementName, e);
-        }
-    }
+			final Model schema = new NamedElement(new Context(xPath, nameTranslator, element)).getModel();
+			schemas.put(schema.getName(), schema.getSchema());
 
-    private void convertReferencedTypes(Node node, TypeTranslator nameTranslator, Schemas schemas) {
-        try {
-            NodeList types = (NodeList) xPath.evaluate(".//@type | .//@itemType", node, XPathConstants.NODESET);
+			convertReferencedElements(element, nameTranslator, schemas);
+			convertExtendedTypes(element, nameTranslator, schemas);
+		}
+		catch (final XPathExpressionException e)
+		{
+			throw new RuntimeException("Error evaluating XPath for named element: " + elementName, e);
+		}
+	}
 
-            for (int i = 0; i < types.getLength(); i++) {
-                String typeName = types.item(i).getNodeValue();
-                convertNamedType(typeName, nameTranslator, schemas);
-            }
-        } catch (XPathExpressionException e) {
-            throw new RuntimeException("Error evaluating XPath for referenced types", e);
-        }
-    }
+	private void convertReferencedTypes(Node node, TypeTranslator nameTranslator, Schemas schemas)
+	{
+		try
+		{
+			final NodeList types = (NodeList) xPath.evaluate(".//@type | .//@itemType", node, XPathConstants.NODESET);
 
-    private void convertExtendedTypes(Node element, TypeTranslator nameTranslator, Schemas schemas) {
-        try {
-            NodeList elements = (NodeList) xPath.evaluate(".//@base", element, XPathConstants.NODESET);
+			for (int i = 0; i < types.getLength(); i++)
+			{
+				final String typeName = types.item(i).getNodeValue();
+				convertNamedType(typeName, nameTranslator, schemas);
+			}
+		}
+		catch (final XPathExpressionException e)
+		{
+			throw new RuntimeException("Error evaluating XPath for referenced types", e);
+		}
+	}
 
-            for (int i = 0; i < elements.getLength(); i++) {
-                convertNamedType(elements.item(i).getNodeValue(), nameTranslator, schemas);
-            }
-        } catch (XPathExpressionException e) {
-            throw new RuntimeException("Error evaluating XPath for extended types", e);
-        }
-    }
+	private void convertExtendedTypes(Node element, TypeTranslator nameTranslator, Schemas schemas)
+	{
+		try
+		{
+			final NodeList elements = (NodeList) xPath.evaluate(".//@base", element, XPathConstants.NODESET);
 
-    private void convertReferencedElements(Node node, TypeTranslator nameTranslator, Schemas schemas) {
-        try {
-            NodeList elements = (NodeList) xPath.evaluate(".//xs:element/@ref", node, XPathConstants.NODESET);
+			for (int i = 0; i < elements.getLength(); i++)
+			{
+				convertNamedType(elements.item(i).getNodeValue(), nameTranslator, schemas);
+			}
+		}
+		catch (final XPathExpressionException e)
+		{
+			throw new RuntimeException("Error evaluating XPath for extended types", e);
+		}
+	}
 
-            for (int i = 0; i < elements.getLength(); i++) {
-                String elementName = elements.item(i).getNodeValue();
-                if (!schemas.containsKey(elementName) && !nameTranslator.isExternalType(elementName)) {
-                    convertNamedElement(elementName, nameTranslator, schemas);
-                }
-            }
-        } catch (XPathExpressionException e) {
-            throw new RuntimeException("Error evaluating XPath for referenced elements", e);
-        }
-    }
+	private void convertReferencedElements(Node node, TypeTranslator nameTranslator, Schemas schemas)
+	{
+		try
+		{
+			final NodeList elements = (NodeList) xPath.evaluate(".//xs:element/@ref", node, XPathConstants.NODESET);
 
-    private void convertSubstituteElements(String elementName, TypeTranslator nameTranslator, Schemas schemas) {
-        try {
-            NodeList elements = (NodeList) xPath.evaluate(
-                "/xs:schema/xs:element[@substitutionGroup='" + elementName + "']/@name",
-                doc,
-                XPathConstants.NODESET
-            );
+			for (int i = 0; i < elements.getLength(); i++)
+			{
+				final String elementName = elements.item(i).getNodeValue();
+				if (!schemas.containsKey(elementName) && !nameTranslator.isExternalType(elementName))
+				{
+					convertNamedElement(elementName, nameTranslator, schemas);
+				}
+			}
+		}
+		catch (final XPathExpressionException e)
+		{
+			throw new RuntimeException("Error evaluating XPath for referenced elements", e);
+		}
+	}
 
-            for (int i = 0; i < elements.getLength(); i++) {
-                convertNamedElement(elements.item(i).getNodeValue(), nameTranslator, schemas);
-            }
-        } catch (XPathExpressionException e) {
-            throw new RuntimeException("Error evaluating XPath for substitute elements", e);
-        }
-    }
+	private void convertSubstituteElements(String elementName, TypeTranslator nameTranslator, Schemas schemas)
+	{
+		try
+		{
+			final NodeList elements = (NodeList) xPath.evaluate("/xs:schema/xs:element[@substitutionGroup='" + elementName + "']/@name", doc,
+					XPathConstants.NODESET);
+
+			for (int i = 0; i < elements.getLength(); i++)
+			{
+				convertNamedElement(elements.item(i).getNodeValue(), nameTranslator, schemas);
+			}
+		}
+		catch (final XPathExpressionException e)
+		{
+			throw new RuntimeException("Error evaluating XPath for substitute elements", e);
+		}
+	}
 }
--- src/test/java/org/cip4/xjdf/json/openapi/JsonSchemaConverterTest.java
+++ src/test/java/org/cip4/xjdf/json/openapi/JsonSchemaConverterTest.java
@@ -1,66 +1,95 @@
 package org.cip4.xjdf.json.openapi;
 
-import com.networknt.schema.*;
-import com.networknt.schema.SpecVersion.VersionFlag;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Locale;
 import java.util.stream.Stream;
 
-public class JsonSchemaConverterTest {
+import org.cip4.jdflib.util.UrlUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-    @Test
-    public void generatedXjdfJsonSchemaIsValid() {
-        JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(VersionFlag.V202012);
-        SchemaValidatorsConfig config = SchemaValidatorsConfig.builder().build();
-        JsonSchema schema = jsonSchemaFactory.getSchema(SchemaLocation.of(SchemaId.V202012), config);
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SchemaId;
+import com.networknt.schema.SchemaLocation;
+import com.networknt.schema.SchemaValidatorsConfig;
+import com.networknt.schema.SpecVersion.VersionFlag;
 
-        SchemaSingleton.assertValid(
-            schema,
-            SchemaSingleton.xjdfSchema.getSchemaNode()
-        );
-    }
+public class JsonSchemaConverterTest
+{
 
-    @Test
-    public void generatedXjmfJsonSchemaIsValid() {
-        JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(VersionFlag.V202012);
-        SchemaValidatorsConfig config = SchemaValidatorsConfig.builder().build();
-        JsonSchema schema = jsonSchemaFactory.getSchema(SchemaLocation.of(SchemaId.V202012), config);
+	static protected final String sm_dirTestData = getTestDataDir();
+	static protected final String sm_dirTestDataTemp = sm_dirTestData + "temp" + File.separator;
 
-        SchemaSingleton.assertValid(
-            schema,
-            SchemaSingleton.xjmfSchema.getSchemaNode()
-        );
-    }
+	private static String getTestDataDir()
+	{
+		return JsonSchemaConverterTest.class.getResource("/data").getPath() + File.separator;
 
-    @ParameterizedTest
-    @MethodSource("scanForFixtures")
-    public void fixturesAreConvertedCorrectly(Path from, Path to) throws Exception {
-        JsonSchemaConverter converter = new JsonSchemaConverter(Files.newInputStream(from));
-        Mapper mapper = new Mapper();
-        Object actualModel = converter.convertModel("Root");
+	}
 
-        Assertions.assertEquals(
-            Files.readString(to),
-            mapper.toYamlString(actualModel)
-        );
-    }
+	@Test
+	public void generatedXjdfJsonSchemaIsValid()
+	{
+		final JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(VersionFlag.V202012);
+		final SchemaValidatorsConfig config = SchemaValidatorsConfig.builder().build();
+		final JsonSchema schema = jsonSchemaFactory.getSchema(SchemaLocation.of(SchemaId.V202012), config);
 
-    public static Stream<Arguments> scanForFixtures() throws Exception {
-        Path fixtureDir = Paths.get(JsonSchemaConverterTest.class.getResource("/converter/").toURI());
+		SchemaSingleton.assertValid(schema, SchemaSingleton.xjdfSchema.getSchemaNode());
+	}
 
-        return Files.walk(fixtureDir)
-            .filter(path -> path.toString().toLowerCase(Locale.getDefault()).endsWith(".xsd"))
-            .map(path -> Arguments.of(
-                path,
-                path.resolveSibling(path.getFileName().toString().replace(".xsd", ".yml"))
-            ));
-    }
+	@Test
+	public void generatedXjmfJsonSchemaIsValid()
+	{
+		final JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(VersionFlag.V202012);
+		final SchemaValidatorsConfig config = SchemaValidatorsConfig.builder().build();
+		final JsonSchema schema = jsonSchemaFactory.getSchema(SchemaLocation.of(SchemaId.V202012), config);
+
+		SchemaSingleton.assertValid(schema, SchemaSingleton.xjmfSchema.getSchemaNode());
+	}
+
+	@ParameterizedTest
+	@MethodSource("scanForFixtures")
+	public void fixturesAreConvertedCorrectly(Path from, Path to) throws Exception
+	{
+		final JsonSchemaConverter converter = new JsonSchemaConverter(Files.newInputStream(from));
+		final Mapper mapper = new Mapper();
+		final Object actualModel = converter.convertModel("Root");
+
+		Assertions.assertEquals(Files.readString(to), mapper.toYamlString(actualModel));
+	}
+
+	public static Stream<Arguments> scanForFixtures() throws Exception
+	{
+		final Path fixtureDir = Paths.get(JsonSchemaConverterTest.class.getResource("/converter/").toURI());
+
+		return Files.walk(fixtureDir).filter(path -> path.toString().toLowerCase(Locale.getDefault()).endsWith(".xsd"))
+				.map(path -> Arguments.of(path, path.resolveSibling(path.getFileName().toString().replace(".xsd", ".yml"))));
+	}
+
+	@Test
+	void testCPICS() throws Exception
+	{
+		final URL schemaUri = new URL(UrlUtil.fileToUrl(new File(sm_dirTestData + "xmlschema/ConventionalPrintingICS1.xsd"), false));
+
+		// Load the XJDF XSD resource
+		final InputStream xjdfXsd = schemaUri.openStream();
+		final String json = sm_dirTestDataTemp + "ConventionalPrintingICS1.json";
+		new File(json).getParentFile().mkdirs();
+		final FileOutputStream xjdfOutput = new FileOutputStream(json);
+
+		// Convert JSON Schema
+		final JsonSchemaConverter jsonConverter = new JsonSchemaConverter(xjdfXsd);
+		jsonConverter.convert(xjdfOutput, null);
+
+	}
+
 }
